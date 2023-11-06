361a83287c633e8271dcbe523a81021bfdd58542
Fernando pereira <fernandomendespereira@hotmail.com>
2023-11-06 15:35:23 -0300

    criando um readMe

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { authOptions } from '@/auth'
 import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
+import gif from "@/images/gif.gif"
 
 export default async function Home() {
 
@@ -47,7 +48,7 @@ export default async function Home() {
               <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                 <Image
                   unoptimized
-                  src=""
+                  src={gif}
                   alt="app Screenshot"
                   width={2432}
                   height={1442}
